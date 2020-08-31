1a0a9d5d2fc7027eaca66ce7b65a580a11d46697
ibrahim ünal <36535542+ibrahimunal@users.noreply.github.com>
2020-09-01 01:40:24 +0300

    Add files via upload

---
+++ Migrations/Expense/20200823064039_MyFirstMigration.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Forms.Migrations.Expense
+{
+    public partial class MyFirstMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ExpenseList",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    type = table.Column<string>(type: "nvarchar(20)", nullable: true),
+                    amount = table.Column<int>(nullable: false),
+                    date = table.Column<DateTime>(nullable: false),
+                    catalouge = table.Column<string>(type: "nvarchar(50)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ExpenseList", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ExpenseList");
+        }
+    }
+}
---
+++ Migrations/Expense/20200827204930_ExpenseUserId.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Forms.Migrations.Expense
+{
+    public partial class ExpenseUserId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "catalouge",
+                table: "ExpenseList",
+                type: "nvarchar(50)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "ExpenseList",
+                type: "nvarchar(20)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "ExpenseList");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "catalouge",
+                table: "ExpenseList",
+                type: "nvarchar(50)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)");
+        }
+    }
+}
